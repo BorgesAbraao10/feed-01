5a7c7c18687333e6c9109aab51148a681d69215f
Abraão Borges <abraao10clear@gmail.com>
2023-11-24 15:47:24 -0300

    Last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
+
 import "./global.css";
 
 export function App() {
@@ -7,12 +10,17 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-        author="Abraão Borges"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et obcaecati amet sapiente, velit laborum inventore fuga, ab reprehenderit eum possimus itaque porro! Sapiente, nobis hic libero at quam vel ipsa."
-      />
+      <div className={styles.wrapper}>
+        <aside></aside>
+        <main>
+          <Post
+            author="Abraão Borges"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et obcaecati amet sapiente, velit laborum inventore fuga, ab reprehenderit eum possimus itaque porro! Sapiente, nobis hic libero at quam vel ipsa."
+          />
 
-      <Post author="Anna Borges" content="Um novo post" />
+          <Post author="Anna Borges" content="Um novo post" />
+        </main>
+      </div>
     </div>
   );
 }
