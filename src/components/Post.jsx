06f21de66889b1852e4421bd773778363c85f074
Abraão Borges <abraao10clear@gmail.com>
2023-11-27 10:32:13 -0300

    Last update

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,7 +15,22 @@ export function Post() {
             <span>Web Developer</span>
           </div>
         </div>
+
+        <time title="27 de Novembro às 18:25" dateTime="2023-11-27 18:25">
+          Publicado há 1h
+        </time>
       </header>
+      <div className={styles.content}>
+        <p>Fala galera!</p>
+        <p>Esse é meu primeiro projeto com React</p>
+        <p>Pode dar uma passadinha lá no meu GitHub para conferir!</p>
+        <p>
+          <a href="https://github.com/BorgesAbraao10">GitHub</a>
+        </p>
+        <p>
+          <a href="#">#Novoprojeto</a>
+        </p>
+      </div>
     </article>
   );
 }
