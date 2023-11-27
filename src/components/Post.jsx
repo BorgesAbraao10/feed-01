06d18c4c197e0dbbf63eb11d8f59a23f73d04f3d
Abraão Borges <abraao10clear@gmail.com>
2023-11-27 08:19:56 -0300

    Last update

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,21 @@
+import styles from "./Post.module.css";
+
 export function Post() {
-  return <h1>Hello world</h1>;
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/BorgesAbraao10.png"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Abraao Borges</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+      </header>
+    </article>
+  );
 }
